9683436abd2a436b38e9c99c145612d5f9681d35
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-27 20:21:16 -0300

    feat: first commit, add dependencies

---
+++ src/main/java/com/example/MottuSystem/MottuSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.MottuSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MottuSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MottuSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MottuSystem/MottuSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MottuSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MottuSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
